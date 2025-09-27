db30ecf214189c0bc1cf78c9bd79fe5ff02efcbe
BalajiPRM <madhanbalajiprm3@gmail.com>
2025-09-27 15:47:34 +0530

    first step on data structures has began

---
+++ Arrays/array_basics.py
@@ -0,0 +1,15 @@
+# array_basics.py
+
+# Creating arrays
+numbers = [1, 2, 3, 4, 5]
+
+# Accessing elements
+print(numbers[0])  # 1
+print(numbers[-1])  # 5
+
+# Slicing
+print(numbers[1:4])  # [2, 3, 4]
+
+# Iterating
+for num in numbers:
+    print(num)
